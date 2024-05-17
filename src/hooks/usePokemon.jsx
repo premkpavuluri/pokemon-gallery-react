7009b94f7faeae4c6762b509e08ff212d74ea973
Prem Pavuluri <52355836+premkpavuluri@users.noreply.github.com>
2024-05-17 23:21:38 +0530

    Prem | Updated usePokemon: Using existing json instead of fetching

--- src/hooks/usePokemon.jsx
+++ src/hooks/usePokemon.jsx
@@ -1,16 +1,21 @@
 import {useEffect, useState} from "react";
+import * as PokemonDetails from '../resources/pokemon.json'
 
 const usePokemon = (pokemonId) => {
   const [pokemon, setPokemon] = useState(null);
 
   useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
-        .then(response => response.json())
-        .then(data => {
-          console.log(data)
-          return data;
-        })
-        .then(({name, sprites: {front_default}}) => setPokemon({name, imageUrl: front_default}));
+    // Commenting because of Bad resolution of the image
+    // fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+    //     .then(response => response.json())
+    //     .then(data => {
+    //       console.log(data)
+    //       return data;
+    //     })
+    //     .then(({name, sprites: {front_default}}) => setPokemon({name, imageUrl: front_default}));
+    const {name, art_url} = PokemonDetails.default[pokemonId - 1];
+
+    setPokemon({name, imageUrl: art_url});
   }, [pokemonId]);
 
   return pokemon;
